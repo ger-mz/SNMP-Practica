faf4e2e99c1a62d372980201c415be12009e2724
Gerardo Martinez <ngermtz@gmail.com>
2022-09-07 19:00:18 -0500

    Agregando Comentarios y limpieza de codigo

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pysnmp.hlapi import *
 from fpdf import FPDF
 import os
 MIB = '1.3.6.1.2.1'
+ip, puerto, comunidad = 0, 0 ,0
 
 def agregarAgente():
     ip = input("Ingresa la ip: ")
@@ -70,25 +71,19 @@ def createIterator(OID, comunidad, ip, puerto):
         ObjectType(ObjectIdentity(OID))
     )
 
-# def getObjeto(iterator):
-#     errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
-#     contador1, contador2 = -1, -1
-#     if errorIndication:
-#         print(errorIndication)
-#     elif errorStatus:
-#         print('%s at %s' % (errorStatus.prettyPrint(),
-#                             errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-#     else:
-#         for varBind in varBinds:
-#             contador1 += 1
-#             for x in varBind:
-#                 contador2 += 1
-#                 # print(x.prettyPrint())
-#                 # print('\n')
-#         return varBinds, contador1, contador2
+def getDatos(OID):
+    global ip, puerto, comunidad
+    iterator = getCmd(
+        SnmpEngine(),
+        CommunityData(comunidad, mpModel=0),
+        # ip to connect, default port for snmp
+        UdpTransportTarget((ip, puerto)),
+        ContextData(),
+        ObjectType(ObjectIdentity(OID))
+    )
 
-def getDatos(iterator):
     errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
     if errorIndication:
         print(errorIndication)
     elif errorStatus:
@@ -96,11 +91,24 @@ def getDatos(iterator):
                             errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
     else:
         return varBinds[0][1]
-        # for varBind in varBinds:
-        #     return varBind[1]
 
 def getAgenteId(ageneteLista, id):
     return ageneteLista[id].split(',')
+
+def pdfGenerator(interfaceInfo):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Times', '', 12)
+    if search('windows', interfaceInfo[1], IGNORECASE):
+        pdf.image('images/Windows8.jpeg', 170, 8, 33)
+
+    if search('ubuntu', interfaceInfo[1], IGNORECASE):
+        pdf.image('images/ubuntu.png', 170, 8, 33)
+
+    for info in interfaceInfo:
+        pdf.cell(0, 10, info, 0, 1)
+    pdfname = interfaceInfo[0]
+    pdf.output(pdfname[pdfname.find(':') + 2:] + '.pdf', 'F')
 
 def generarReporte():
     # seleccionar agente, enviar solicitudes snmp para solicitar informacion
@@ -114,19 +122,17 @@ def generarReporte():
         return
 
     print('')
+
+    global ip, puerto, comunidad
     ip, puerto, comunidad = getAgenteId(lista, agente-1)
-    ip = ip.strip()
-    puerto = puerto.strip()
-    comunidad = comunidad.strip()
+    ip, puerto, comunidad = ip.strip(), puerto.strip(), comunidad.strip()
 
     # nombre / hostname
-    iterator = createIterator(MIB+'.1.5.0', comunidad, ip, puerto)
-    interfaceInfo.append('Hostname: '+str(getDatos(iterator)))
+    interfaceInfo.append('Hostname: '+str(getDatos(MIB+'.1.5.0')))
     print(interfaceInfo[0])
 
     # S.O (version, logo), solo funciona con windows y ubuntu
-    iterator = createIterator(MIB+'.1.1.0', comunidad, ip, puerto)
-    sistemaOperativo = str(getDatos(iterator)).split()
+    sistemaOperativo = str(getDatos(MIB+'.1.1.0')).split()
     for x in sistemaOperativo:
         if search('windows', x, IGNORECASE) or search('ubuntu', x, IGNORECASE):
             interfaceInfo.append('Sistema Operativo: ' + x)
@@ -134,13 +140,11 @@ def generarReporte():
             break
 
     # ubicacion
-    iterator = createIterator(MIB+'.1.6.0', comunidad, ip, puerto)
-    interfaceInfo.append('Ubicacion: ' + str(getDatos(iterator)))
+    interfaceInfo.append('Ubicacion: ' + str(getDatos(MIB+'.1.6.0')))
     print(interfaceInfo[2])
 
     # numero de interfaces
-    iterator = createIterator(MIB+'.2.1.0', comunidad, ip, puerto)
-    numeroIntefaces = getDatos(iterator)
+    numeroIntefaces = getDatos(MIB+'.2.1.0')
     interfaceInfo.append('Numero de Interfaces: '+ str(numeroIntefaces))
     print(interfaceInfo[3])
 
@@ -148,35 +152,20 @@ def generarReporte():
     # Nombre de las interfaces
     print('Estatus de las interfaces: ')
     for i in range(1, int(numeroIntefaces)+1):
-        iterator = createIterator((MIB+'.2.2.1.2.'+str(i)), comunidad, ip, puerto)
-        name = getDatos(iterator)
-        iterator = createIterator((MIB + '.2.2.1.7.' + str(i)), comunidad, ip, puerto)
-        status = getDatos(iterator)
+        name = getDatos(MIB+'.2.2.1.2.'+str(i))
+        status = getDatos(MIB + '.2.2.1.7.' + str(i))
         interfaceInfo.append('Interface %d: %s' % (i, str(name)) +
-              ' Estatus:' + ('up' if status == 1 else 'down' if status == 2 else 'testing'))
+              ' Estatus: ' + ('up' if status == 1 else 'down' if status == 2 else 'testing'))
         print(interfaceInfo[(3+i)])
         if i > 4:
             break
 
     # tiempo de actividad
-    iterator = createIterator('1.3.6.1.2.1.1.3.0', comunidad, ip, puerto)
-    interfaceInfo.append('Tiempo de Actividad: %d %s' %(getDatos(iterator)//6000, 'Minutos'))
+    interfaceInfo.append('Tiempo de Actividad: %d %s' %(getDatos(MIB+'.1.3.0')//6000, 'Minutos'))
     print(interfaceInfo[len(interfaceInfo)-1])
 
-    pdf = FPDF()
-    pdf.add_page()
-    # vary = 20
-    pdf.set_font('Times', '', 12)
-    if search('windows', interfaceInfo[1], IGNORECASE):
-        pdf.image('images/Windows8.jpeg', 170, 8, 33)
-
-    if search('ubuntu', interfaceInfo[1], IGNORECASE):
-        pdf.image('images/ubuntu.png', 170, 8, 33)
-
-    for info in interfaceInfo:
-        pdf.cell(0, 10, info, 0, 1)
-    pdfname = interfaceInfo[0]
-    pdf.output(pdfname[pdfname.find(':')+2:]+'.pdf', 'F')
+    # Generador de reporte pdf con el nombre del hostname
+    pdfGenerator(interfaceInfo)
 
     print('\nProcess Succesfull\n')
 
